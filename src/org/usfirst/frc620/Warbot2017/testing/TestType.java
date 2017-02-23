f7126c0e1619be8bd0ab75612e9af09873f171d9
Mike <mcombsb1@gmail.com>
2017-02-23 18:05:16 -0500

    Added Test FrameWork

---
+++ src/org/usfirst/frc620/Warbot2017/testing/TestType.java
@@ -0,0 +1,5 @@
+package org.usfirst.frc620.Warbot2017.testing;
+
+public enum TestType {
+
+}
---
+++ src/org/usfirst/frc620/Warbot2017/testing/TestingSuite.java
@@ -0,0 +1,9 @@
+package org.usfirst.frc620.Warbot2017.testing;
+
+public class TestingSuite {
+	public TestingSuite() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
+
