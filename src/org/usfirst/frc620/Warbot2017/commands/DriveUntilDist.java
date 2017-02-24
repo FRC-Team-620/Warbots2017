fe6a5774bd287840cb9339bbd3634b80aa5cdf41
Liz <liz.weech@gmail.com>
2017-02-23 23:28:30 -0500

    Updated Drive until dist to use navx and pid loop Also added util
    pid class

--- src/org/usfirst/frc620/Warbot2017/commands/DriveUntilDist.java
+++ src/org/usfirst/frc620/Warbot2017/commands/DriveUntilDist.java
@@ -1,7 +1,9 @@
 package org.usfirst.frc620.Warbot2017.commands;
 
 import org.usfirst.frc620.Warbot2017.Robot;
+import org.usfirst.frc620.Warbot2017.util.DummyPIDOutput;
 
+import edu.wpi.first.wpilibj.PIDController;
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
@@ -13,32 +15,66 @@ import edu.wpi.first.wpilibj.command.Command;
 public class DriveUntilDist extends Command {
 	private int dist;
 	private double k;
+	private PIDController distController;
+	private static final double DIST_TOLERANCE = 2;
+	private static final double DIST_P = 0.03;
+	private static final double DIST_I = 0.00;
+	private static final double DIST_D = 0.00;
+	private static final double DIST_F = 0.00;
+	private DummyPIDOutput distOutput;
+	private PIDController turnController;
+	private static final double TURN_TOLERANCE = 2;
+	private static final double TURN_P = 0.03;
+	private static final double TURN_I = 0.00;
+	private static final double TURN_D = 0.00;
+	private static final double TURN_F = 0.00;
+	private DummyPIDOutput turnOutput;
 
     public DriveUntilDist(int distInCentimeters, double scaling) {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
     	requires(Robot.driveTrain);
     	dist = distInCentimeters;
-    	if(scaling < .25) scaling = .25;
-    	k = scaling;
+//    	if(scaling < .25) scaling = .25;
+//    	k = scaling;
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
+    	turnOutput = new DummyPIDOutput();
+    	turnController = new PIDController(TURN_P, TURN_I,TURN_D, TURN_F, Robot.navX.navX, turnOutput);
+		turnController.setInputRange(-180.0, 180.0);
+		turnController.setOutputRange(-1.0, 1.0);
+		turnController.setAbsoluteTolerance(TURN_TOLERANCE);
+		turnController.setContinuous(true);
+		turnController.setSetpoint(Robot.navX.getYaw());
+		
+		distOutput = new DummyPIDOutput();
+		distController = new PIDController(DIST_P, DIST_I,DIST_D, DIST_F, Robot.navX.navX, turnOutput);
+		distController.setInputRange(0, 500);
+		distController.setOutputRange(-1.0, 1.0);
+		distController.setAbsoluteTolerance(DIST_TOLERANCE);
+		distController.setContinuous(false);
+		distController.setSetpoint(dist);
+		
+		turnController.enable();
+		distController.enable();
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	Robot.driveTrain.mecanumDrive(0.0, 1.0 * k, 0.0, 0.0);
+    	Robot.driveTrain.mecanumDrive(turnOutput.getOutput(), distOutput.getOutput() * k, 0.0, 0.0);
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return Robot.lidar.getDistance() <= dist;
+        return distController.onTarget();
     }
 
     // Called once after isFinished returns true
     protected void end() {
+    	distController.disable();
+    	turnController.disable();
     }
 
     // Called when another command which requires one or more of the same
---
+++ src/org/usfirst/frc620/Warbot2017/util/DummyPIDOutput.java
@@ -0,0 +1,28 @@
+package org.usfirst.frc620.Warbot2017.util;
+
+import edu.wpi.first.wpilibj.PIDOutput;
+
+/**
+ * A dummy PID output that does not output to any hardware. Pass the
+ * DummyPIDOutput object to the PIDController class and read the output in your
+ * own code. This is useful for mixing the results of multiple PID controllers
+ * to one output.
+ *
+ * @author Joe Ross, Team 330
+ */
+public class DummyPIDOutput implements PIDOutput {
+	double output;
+
+	public DummyPIDOutput() {
+		output = 0;
+	}
+
+	public void pidWrite(double output) {
+		this.output = output;
+	}
+
+	public double getOutput() {
+		return output;
+	}
+
+}
