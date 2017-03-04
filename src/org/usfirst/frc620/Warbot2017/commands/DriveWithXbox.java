5f93568095c3136c71efe790c34a176cba9723d0
Mike <mcombsb1@gmail.com>
2017-03-04 11:15:14 -0500

    Made it compile

--- src/org/usfirst/frc620/Warbot2017/commands/DriveWithXbox.java
+++ src/org/usfirst/frc620/Warbot2017/commands/DriveWithXbox.java
@@ -24,7 +24,7 @@ public class DriveWithXbox extends Command {
 	// Called repeatedly when this Command is scheduled to run
 	protected void execute() {
 		
-		Robot.cameras.brightenCamera();
+		Robot.cameras.brightenCamera(0);
 		
 		float temp= Robot.navX.getYaw();
 //		System.out.println(timeSinceInitialized()+"NavX "+temp);
