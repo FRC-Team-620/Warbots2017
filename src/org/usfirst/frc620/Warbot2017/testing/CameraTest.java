11dd57ae6ace56b0e04346ad6abf8967628653ca
m10653 <mcombsb1@gmail.com>
2017-03-08 21:45:12 -0500

    Might Have come up with a fix for camera switching

--- src/org/usfirst/frc620/Warbot2017/testing/CameraTest.java
+++ src/org/usfirst/frc620/Warbot2017/testing/CameraTest.java
@@ -1,67 +1,51 @@
 package org.usfirst.frc620.Warbot2017.testing;
 
-import java.util.concurrent.atomic.AtomicInteger;
-
+import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.videoio.VideoCapture;
 import org.usfirst.frc620.Warbot2017.Robot;
 
-import edu.wpi.cscore.CvSink;
 import edu.wpi.cscore.CvSource;
-import edu.wpi.cscore.MjpegServer;
-import edu.wpi.cscore.UsbCamera;
-import edu.wpi.cscore.UsbCameraInfo;
 import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.XboxController;
 
 public class CameraTest {
 	private VideoCapture[] cams;
-//	private AtomicInteger id =
-	public CameraTest() {
-//		MjpegServer test = new MjpegServer("Cam", 0);
-		cams = new VideoCapture[2];
-		for(int i = 0 ; i < cams.length; i ++){
-			cams[i] = new VideoCapture(i);
-		}
-		
-//		VideoCapture cam = new VideoCapture(0);
-//		Mat in = new Mat();
-//		cam.read(in);
-//		test.getSource().
-//		cam.
-//		test.setSource(cam);
-//		test.setSource(source);
-		new Thread(() -> {
-//			UsbCamera camera = CameraServer.getInstance().startAutomaticCapture();
-//			camera.setResolution(640, 480);
 
-//			CvSink cvSink = CameraServer.getInstance().getVideo();
-			CvSource outputStream = CameraServer.getInstance().putVideo("Cam", 640, 480);
+	public CameraTest() {
+		Mat backGround = new Mat(482,642, CvType.CV_8UC3, new Scalar(0)); //Create A black Image with 8 Color bits with 3 channels 
+		cams = new VideoCapture[2];
+		for (int i = 0; i < cams.length; i++) {
+			cams[i] = new VideoCapture(i);
+			//			cams[i].set(Videoio.CAP_PROP_FRAME_WIDTH, 640);
+			//			cams[i].set(Videoio.CAP_PROP_FRAME_HEIGHT, 480);			
+		}
+
+		new Thread(() -> {
+			CvSource outputStream = CameraServer.getInstance().putVideo("Cam", 642, 482);
 
 			Mat source = new Mat();
 			Mat output = new Mat();
 			XboxController xbox = Robot.oi.getXbox();
 			while (!Thread.interrupted()) {
-//				cvSink.grabFrame(source);
-				
 				System.out.println(!xbox.getAButton());
-				if(!xbox.getAButton()){
+				if (!xbox.getAButton()) {
 					cams[0].read(source);
-				}else{
+				} else {
 					cams[1].read(source);
 				}
+				backGround.copyTo(output);
+				Imgproc.resize(source, source, new Size(620, 480));
+				source.copyTo(output.rowRange(1,481).colRange(1,621));
 				System.out.println("Source Mat: Channels: " + source.channels());
 				System.out.println("Source Mat: Depth: " + source.depth());
 				System.out.println("Source Mat: height: " + source.height());
 				System.out.println("Source Mat: width: " + source.width());
-				System.out.println("Source Mat: dump: " + source.dump());
 				System.out.println("-----------------------------------");
-//				Imgproc.
-//				Imgproc.
-//				source.
-				Imgproc.cvtColor( source, output, Imgproc.COLOR_BGRA2BGR);
-				Imgproc.cvtColor(source, output, Imgproc.COLOR_BGR2GRAY);
+
 				outputStream.putFrame(output);
 			}
 		}).start();
